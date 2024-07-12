9dfe610b8d2205164ac93752edca682d7eebe7ab
Yann 'Ze' Richard <Zeuh@users.noreply.github.com>
2024-07-12 13:32:01 +0200

    Fix: explicit python3 shebang
    replace shebang python with python3

--- bin/pynagsystemd.py
+++ bin/pynagsystemd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 You can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation, either version 2
